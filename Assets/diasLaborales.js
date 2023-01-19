71b212e34a319a5b2c49e1c6292af39c1c6defcd
Juan Marcos Gonzalez <gonzalezromojm@hotmail.com>
2023-01-19 00:41:04 -0600

    ci: Estilos

--- Assets/diasLaborales.js
+++ Assets/diasLaborales.js
@@ -1,34 +1,73 @@
-function calculaFecha(){
-    
+/** Funcion para delimitar numero de dias
+ * 
+ * @param {*} anioComprobacion año obtenido en input
+ * @param {*} mesComprobacion mes obtenido en input
+ * @returns numero de dias por mes
+ */
+const comprobacionDias = (anioComprobacion, mesComprobacion) => {
+    let numeroDias = 0;
+    switch (mesComprobacion) {
+        case 0: numeroDias = 31; break;
+        case 1: anioComprobacion % 4 == 0 ? numeroDias = 29 : numeroDias = 28; break;
+        case 2: numeroDias = 31; break;
+        case 3: numeroDias = 30; break;
+        case 4: numeroDias = 31; break;
+        case 5: numeroDias = 30; break;
+        case 6: numeroDias = 31; break;
+        case 7: numeroDias = 31; break;
+        case 8: numeroDias = 30; break;
+        case 9: numeroDias = 31; break;
+        case 10: numeroDias = 30; break;
+        case 11: numeroDias = 31; break;
+    }
+    return numeroDias;
+}
+/** Obtener dia de la semana
+ * 
+ * @returns numero de dia de la semana
+ */
+const obtenerDia = () => {
+    let anio = parseInt(document.getElementById("anio").value);
+    let mes = parseInt(document.getElementById("meses").value);
+    let numeroDiasMes = comprobacionDias(anio, mes);
+    document.getElementById("dia").setAttribute("max", numeroDiasMes);
     let dia = document.getElementById("dia").value;
-    let anio = document.getElementById("anio").value;
-    let mes = document.getElementById("meses").value;
-    const meses =["ene","feb","mar","abr","may","jun","jul","ago","sep","oct","nov","dic"]
-    let numMes= meses.indexOf(mes);
-    let obtencionFecha = new Date (anio, numMes, dia);
-    console.log(obtencionFecha);
-    let numDiaSemana = obtencionFecha.getDay();
-
-    mostrarResultado(numDiaSemana)
-    
-    console.log(`Dia: ${dia} Mes: ${mes},${numMes} Año: ${anio} Dia semanal ${numDiaSemana}`);
-    console.log(typeof(numDiaSemana));
+    if(dia<=numeroDiasMes){
+        let obtencionFecha = new Date(anio, mes, dia);
+        return numDiaSemana = obtencionFecha.getDay();
+    }
+    else {
+        alert("Elegiste un numero de dia que no coincide con el mes solicitado");
+    }
 }
 
-const mostrarResultado = (numDia) =>{
-    let diasLaborales="";
-    switch(numDia){
-        case 1: if(numDia === 1) diasLaborales = "Lunes";
-        case 2: if(numDia === 2) diasLaborales = "Martes";
-        case 3: if(numDia === 3) diasLaborales = "Miercoles";
-        case 4: if(numDia === 4) diasLaborales = "Jueves";
-        case 5: if(numDia === 5) diasLaborales = "Viernes";
-        diasLaborales+=", dia laborable";
-        break;
-        case 6: if(numDia === 6) diasLaborales = "Sabado";
-        case 0: if(numDia === 0) diasLaborales = "Domingo";
-        diasLaborales+=", dia no laborable"
-        break;
+/**
+ * 
+ * @param {*} numDia valor numerico de dia de la semana
+ * @returns string con dia de la semana y si es laboral
+ */
+const obtenerDiaLaboral = (numDia) => {
+    let diaSemLab = "";
+    switch (numDia) {
+        case 1: if (numDia === 1) diaSemLab = "Lunes";
+        case 2: if (numDia === 2) diaSemLab = "Martes";
+        case 3: if (numDia === 3) diaSemLab = "Miercoles";
+        case 4: if (numDia === 4) diaSemLab = "Jueves";
+        case 5: if (numDia === 5) diaSemLab = "Viernes";
+        diaSemLab += ", dia laborable";
+            break;
+        case 6: if (numDia === 6) diaSemLab = "Sabado";
+        case 0: if (numDia === 0) diaSemLab = "Domingo";
+        diaSemLab += ", dia no laborable"
+            break;
     }
-    document.getElementById("diaLaboral").innerHTML=diasLaborales;
+    return diaSemLab;
+}
+/**
+ * Funcion principal donde se llaman demas funciones.
+ */
+function calculaFechaLaboral() {
+    const numDia = obtenerDia();
+    const diaLaboralSemanal = obtenerDiaLaboral(numDia);
+    document.getElementById("diaLaboral").innerHTML = diaLaboralSemanal;
 }
